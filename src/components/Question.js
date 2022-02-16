dfd747b1d35cb75c8810ec6af8f1e1cd790c7b9b
Kahlia Pinkins <kahliapinkins123@gmail.com>
2022-02-15 19:03:56 -0500

    commit tree fbb7c7eeff7ccec9696e345576f687f723ca6faa

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,14 +6,16 @@ function Question({ question, onAnswered }) {
   // add useEffect code
   useEffect(()=>{
     if(timeRemaining === 0){
-      setTimeRemaining(10)
-      onAnswered(false)
-    } else{
-      setTimeout(() => setTimeRemaining(()=>timeRemaining -1), 1000);
-    }
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    } 
+
+    const timer = setTimeout(() => setTimeRemaining(()=>timeRemaining -1), 1000);
+    
     // returning a cleanup function
-    return function cleanup() {
-      
+    return function() {
+      clearTimeout(timer)
     };
  
   },[timeRemaining,onAnswered])
